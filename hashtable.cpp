a77374a3505b3d4f5eb604cec73870d3459bc2d0
Quoc Cao <quoccao0101@gmail.com>
2026-09-20 13:44:43 +0700

    scholarship backend relocate true file

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,11 +1,1 @@
-﻿class ScholarshipSystem {
-private:
-    // 1. Hash Table - tra cứu theo MSSV
-    unordered_map<string, HocSinh*> by_mssv;
-
-    // 2. Set - xếp hạng theo nhiều tiêu chí
-    set<HocSinh*, CmpHocBong> by_priority;
-
-public:
-    // các hàm...
-};
+﻿
